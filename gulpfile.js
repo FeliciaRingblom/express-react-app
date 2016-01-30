f43d400cbb87fe84ff3a19074dd725bac1ab161e
Felicia Ringblom <felicia.ringblom@gmail.com>
2016-01-30 21:11:54 +0100

    added babelify gulp process

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
 var gulp = require('gulp');
 var LiveServer = require('gulp-live-server');
 var browserSync = require('browser-sync');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var babelify = require('babelify');
 
 gulp.task('live-server', function(){
   var server = new LiveServer('server/server.js');
   server.start();
+})
+
+gulp.task('bundle', function(){
+  return browserify({
+    entries: 'app/main.js',
+    debug: true
+  })
+  .transform(babelify, { presets: ['es2015', 'react'] })
+  .bundle()
+  .pipe(source('bundle.js'))
+  .pipe(gulp.dest('public/js'))
 })
 
 gulp.task('serve', ['live-server'], function(){
