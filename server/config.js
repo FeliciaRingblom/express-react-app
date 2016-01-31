f55b437ebbdaa63f198401e73581960bd53f32cc
Felicia Ringblom <felicia.ringblom@gmail.com>
2016-01-31 11:45:02 +0100

    added api and connection to mongodb

---
+++ server/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  dbUrl: 'mongodb://felri:banan123@ds035573.mongolab.com:35573/shareideas_db'
+};
--- server/server.js
+++ server/server.js
@@ -1,9 +1,40 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+
+var config = require('./config');
 
 var app = new express();
+
+mongoose.connect(config.dbUrl);
+mongoose.connection.on('error', function() {
+  console.info('Error: Could not connect to MongoDB');
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/../public/js'));
+
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+	next();
+});
 
 app.get('/',function(req,res){
     res.render('./../app/index.ejs',{});
 })
-.use(express.static(__dirname + '/../public/js'))
-.listen(7777);
+
+
+var apiRouter = express.Router();
+
+apiRouter.get('/', function(req, res) {
+  res.json({ message: 'hooray! welcome to our api!' });
+});
+
+app.use('/api', apiRouter);
+
+app.listen(7777);
